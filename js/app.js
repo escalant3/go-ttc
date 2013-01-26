a4db0d3bf602d3834cb6b63a44d740fa26e3c213
Gregory Pike <greg@gregpike.ca>
2013-01-26 16:49:39 -0500

    Loader

--- js/app.js
+++ js/app.js
@@ -256,6 +256,35 @@ angular.module("GoTTC", [])
       };
     }
 ])
+.directive('loading', [
+  function() {
+  return {
+    template: '<div ng-show="showLoading"><div class="loading-base"><img src="img/loading.gif" height="{{size}}"></div><p class="loading-message" ng-show="message" ng-bind-html-unsafe="message"></p></div>',
+    link: function(scope, element, attrs) {
+
+      scope.showLoading = true;
+      scope.message = null;
+      scope.size = 64;
+
+      if (!!attrs.message) {
+        scope.message = attrs.message;
+      }
+
+      if (!!attrs.size) {
+        scope.size = attrs.size;
+      }
+
+      scope.$watch(attrs.showLoading, function(value){
+        if (!!value) {
+          scope.showLoading = true;
+          return;
+        }
+        scope.showLoader = false;
+      });
+
+    }
+  };
+}])
 .filter('getRouteName',function() {
     return function(name) {
         if (!name) return '';
